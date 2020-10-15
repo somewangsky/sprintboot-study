2617305d86b204e5c901dd08d9cf00b05354fdfb
汪盛民 <wang.shengmin@zxelec.com>
2020-10-15 17:20:30 +0800

    first commit

---
+++ src/main/java/com/zxelec/SpringbootStudyApplication.java
@@ -0,0 +1,13 @@
+package com.zxelec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zxelec/SpringbootStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zxelec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
