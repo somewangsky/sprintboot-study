bfe41fd98d12e2b91050bb2a66434cab57ec535f
汪盛民 <wang.shengmin@zxelec.com>
2020-10-15 17:49:45 +0800

    添加实体类

--- src/main/java/com/zxelec/SpringbootStudyApplication.java
+++ src/main/java/com/zxelec/SpringbootStudyApplication.java
@@ -1,8 +1,13 @@
 package com.zxelec;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author vimicro
+ */
+@MapperScan(value = {"com.zxelec.cache"})
 @SpringBootApplication
 public class SpringbootStudyApplication {
 
